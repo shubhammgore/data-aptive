08f11536a0e8e8f3828614ac206db7081bf56d5a
shubham <shubh.shubham21@gmail.com>
2022-05-24 16:00:56 +0530

    navbar, hero, home and routing added

---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+import Hero from '../hero/Hero';
+
+const Home = () => {
+	return (
+		<div>
+			<Navbar />
+			<Hero />
+		</div>
+	);
+};
+
+export default Home;
