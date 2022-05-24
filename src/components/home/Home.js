bf158b84aa222dca47b2ba3293e5a7922565bf55
shubham <shubh.shubham21@gmail.com>
2022-05-24 18:37:23 +0530

    pricing and testimonials sections added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../navbar/Navbar';
 import Hero from '../hero/Hero';
+import Pricing from '../pricing/Pricing';
+import Testimonials from '../testimonials/Testimonials';
 
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
 			<Hero />
+			<Pricing />
+			<Testimonials />
 		</div>
 	);
 };
